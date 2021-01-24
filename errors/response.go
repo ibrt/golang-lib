f934ab5857f16b7a4c3bb2137bb1b8dbaff73ccd
Ivan Bertona <ivan@ibrt.me>
2021-01-24 12:22:43 -0500

    Add error response.

--- errors/response.go
+++ errors/response.go
@@ -1,1 +1,21 @@
 package errors
+
+// ErrorResponse describes an error response.
+type ErrorResponse struct {
+	ErrorID    ID                     `json:"errorId,omitempty"`
+	StatusCode StatusCode             `json:"statusCode,omitempty"`
+	Metadata   map[string]interface{} `json:"metadata,omitempty"`
+	Message    string                 `json:"message,omitempty"`
+	StackTrace []string               `json:"stackTrace,omitempty"`
+}
+
+// ToResponse converts an error to ErrorResponse.
+func ToResponse(err error) *ErrorResponse {
+	return &ErrorResponse{
+		ErrorID:    GetID(err),
+		StatusCode: GetStatusCode(err),
+		Metadata:   GetMetadata(err),
+		Message:    err.Error(),
+		StackTrace: FormatStackTrace(GetCallers(err)),
+	}
+}
---
+++ errors/response_test.go
@@ -0,0 +1,20 @@
+package errors_test
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/ibrt/golang-lib/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestToResponse(t *testing.T) {
+	resp := errors.ToResponse(errors.Errorf("some error", errors.Prefix("prefix"), errors.ID("id"), errors.StatusCode(http.StatusUnauthorized)))
+	require.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+	require.Equal(t, "id", resp.ErrorID)
+	require.Equal(t, "prefix: some error", resp.Message)
+	require.NotEmpty(t, resp.StackTrace)
+	require.True(t, strings.HasPrefix(resp.StackTrace[0], "errors_test.TestToResponse"))
+
+}
