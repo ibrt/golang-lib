95f627d651ecb3f56c265b90085e4ed7c9bfff51
Ivan Bertona <ivan.bertona@gmail.com>
2024-11-21 23:36:59 +0100

    Add internal tooling.

---
+++ internal/validate/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"path/filepath"
+
+	"github.com/ibrt/golang-lib/consolez"
+	"github.com/ibrt/golang-lib/devz"
+)
+
+func main() {
+	consolez.DefaultCLI.Banner("golang-lib", "Validate")
+
+	devz.MustRunGoChecks(&devz.GoChecksParams{
+		Packages:      []string{"./..."},
+		PrintNotices:  true,
+		PrintCommands: true,
+	})
+
+	devz.MustRunGoTests(&devz.GoTestsParams{
+		Packages:        []string{"./..."},
+		CoverageDirPath: filepath.Join(".build", "coverage"),
+		OpenCoverage:    false,
+		PrintNotices:    true,
+		PrintCommands:   true,
+	})
+}
