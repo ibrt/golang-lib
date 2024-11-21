1260b39cc5abaf8b8da458f2fd3d309dcce02d99
Ivan Bertona <ivan.bertona@gmail.com>
2024-11-21 17:47:17 +0100

    Continue adding consolez package.

---
+++ consolez/gotest_printer.go
@@ -0,0 +1,181 @@
+package consolez
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+
+	"github.com/ibrt/golang-lib/outz"
+	"github.com/ibrt/golang-lib/stringz"
+)
+
+// GoTestPrinter implements an improved printer for "go test" output.
+type GoTestPrinter interface {
+	PrintLine(line string)
+	PrintDone()
+}
+
+type goTestPrinter struct {
+	startTime time.Time
+	passPkgs  int
+	skipPkgs  int
+	maxPkgLen int
+}
+
+// NewGoTestPrinter initializes a new GoTestPrinter.
+func NewGoTestPrinter() GoTestPrinter {
+	return &goTestPrinter{
+		startTime: time.Now(),
+		passPkgs:  0,
+		maxPkgLen: 60,
+	}
+}
+
+// PrintLine implements the GoTestPrinter interface.
+func (p *goTestPrinter) PrintLine(line string) {
+	trimmedLine := strings.TrimSpace(line)
+
+	switch {
+	case p.maybeHandleSummaryLine(line), strings.HasPrefix(line, "coverage:"):
+		// do nothing
+	case p.isSecondary(trimmedLine):
+		_, _ = outz.GetColorSecondary().Print(line)
+		fmt.Print("\n")
+	case p.isSuccess(trimmedLine):
+		_, _ = outz.GetColorSuccess().Print(line)
+		fmt.Print("\n")
+	case p.isError(trimmedLine):
+		_, _ = outz.GetColorError().Print(line)
+		fmt.Print("\n")
+	case p.isHighlight(trimmedLine):
+		_, _ = outz.GetColorHighlight().Print(line)
+		fmt.Print("\n")
+	default:
+		_, _ = fmt.Println(line)
+	}
+}
+
+// PrintDone prints a final line.
+func (p *goTestPrinter) PrintDone() {
+	fmt.Printf(
+		fmt.Sprintf("DONE    %%-%vv %%-10s\n", p.maxPkgLen),
+		fmt.Sprintf("[SKIP: %v, PASS: %v]", p.skipPkgs, p.passPkgs),
+		time.Since(p.startTime).Truncate(time.Millisecond*10))
+}
+
+func (p *goTestPrinter) maybeHandleSummaryLine(line string) bool {
+	var pfx string
+	var clr *color.Color
+
+	switch {
+	case strings.HasPrefix(line, "?   \t"), strings.HasPrefix(line, "\t"):
+		pfx = "SKIP"
+		clr = outz.GetColorSecondary()
+	case strings.HasPrefix(line, "ok  \t"):
+		pfx = "PASS"
+		clr = outz.GetColorSuccess()
+	case strings.HasPrefix(line, "FAIL\t"):
+		pfx = "FAIL"
+		clr = outz.GetColorError()
+	default:
+		return false
+	}
+
+	rawLineParts := strings.Split(line, "\t")
+	lineParts := make([]string, 0, len(rawLineParts))
+	hasCoverage := false
+
+	for _, rawLinePart := range rawLineParts {
+		linePart := strings.TrimSpace(rawLinePart)
+
+		if linePart == "" {
+			continue
+		} else if strings.HasPrefix(linePart, "coverage:") {
+			hasCoverage = true
+			continue
+		} else if d, err := time.ParseDuration(linePart); err == nil {
+			linePart = fmt.Sprintf("%-10s", d.Truncate(time.Millisecond*10))
+		} else if linePart == "[no test files]" {
+			linePart = "[no tests]"
+		}
+
+		lineParts = append(lineParts, linePart)
+	}
+
+	if len(lineParts) == 1 && hasCoverage {
+		lineParts = []string{
+			"",
+			lineParts[0],
+			"[no tests]",
+		}
+	}
+
+	if len(lineParts) < 3 {
+		return false
+	}
+
+	if pfx == "SKIP" {
+		p.skipPkgs++
+	}
+
+	if pfx == "PASS" {
+		p.passPkgs++
+	}
+
+	_, _ = clr.Printf(
+		fmt.Sprintf("%%v    %%-%vv %%v", p.maxPkgLen),
+		pfx,
+		stringz.AlignRight(lineParts[1], p.maxPkgLen),
+		strings.Join(lineParts[2:], " "))
+	fmt.Print("\n")
+
+	return true
+}
+
+func (p *goTestPrinter) isSecondary(trimmedLine string) bool {
+	switch {
+	case strings.HasPrefix(trimmedLine, "--- SKIP"),
+		trimmedLine == "SKIP",
+		strings.HasPrefix(trimmedLine, "-test.shuffle"):
+		return true
+	default:
+		return false
+	}
+}
+
+func (p *goTestPrinter) isSuccess(trimmedLine string) bool {
+	switch {
+	case strings.HasPrefix(trimmedLine, "--- PASS"),
+		trimmedLine == "PASS":
+		return true
+	default:
+		return false
+	}
+}
+
+func (p *goTestPrinter) isError(trimmedLine string) bool {
+	switch {
+	case strings.HasPrefix(trimmedLine, "--- FAIL"),
+		trimmedLine == "FAIL",
+		strings.Contains(trimmedLine, "no tests to run"):
+		return true
+	default:
+		return false
+	}
+}
+
+func (p *goTestPrinter) isHighlight(trimmedLine string) bool {
+	switch {
+	case strings.HasPrefix(trimmedLine, "=== RUN"),
+		strings.Contains(trimmedLine, "[BeforeSuite]"),
+		strings.Contains(trimmedLine, "[AfterSuite]"),
+		strings.Contains(trimmedLine, "[BeforeTest]"),
+		strings.Contains(trimmedLine, "[AfterTest]"),
+		strings.Contains(trimmedLine, "[TestMethod]"):
+		return true
+	default:
+		return false
+	}
+}
---
+++ consolez/gotest_printer_test.go
@@ -0,0 +1,79 @@
+package consolez_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/ibrt/golang-lib/consolez"
+	"github.com/ibrt/golang-lib/fixturez"
+	"github.com/ibrt/golang-lib/outz"
+)
+
+type GoTestPrinterSuite struct {
+	// intentionally empty
+}
+
+func TestGoTestPrinterSuite(t *testing.T) {
+	fixturez.RunSuite(t, &GoTestPrinterSuite{})
+}
+
+func (*GoTestPrinterSuite) TestGoTestPrinter(g *WithT) {
+	outz.MustStartCapturing(outz.SetupStandardStreams, outz.SetupColorStreams, outz.SetupTableStreams)
+	defer outz.MustResetCapturing()
+
+	p := consolez.NewGoTestPrinter()
+	p.PrintLine("other")
+	p.PrintLine("coverage: 100%")
+	p.PrintLine("--- SKIP")
+	p.PrintLine("    --- SKIP")
+	p.PrintLine("SKIP")
+	p.PrintLine("    SKIP")
+	p.PrintLine("-test.shuffle=1234")
+	p.PrintLine("--- PASS")
+	p.PrintLine("    --- PASS")
+	p.PrintLine("PASS")
+	p.PrintLine("    PASS")
+	p.PrintLine("--- FAIL")
+	p.PrintLine("    --- FAIL")
+	p.PrintLine("FAIL")
+	p.PrintLine("    FAIL")
+	p.PrintLine("=== RUN")
+	p.PrintLine("    === RUN")
+	p.PrintLine("?   \tpkgn\t[no test files]")
+	p.PrintLine("?   \tunexpected")
+	p.PrintLine("ok  \tpkgn\t1s\tcoverage: 100%")
+	p.PrintLine(fmt.Sprintf("FAIL\t%v\t1s", strings.Repeat("p", 1024)))
+	p.PrintDone()
+
+	outBuf, errBuf := outz.MustStopCapturing()
+
+	g.Expect(outBuf).To(Equal(strings.Join([]string{
+		"other",
+		"\x1b[2m--- SKIP\x1b[0m",
+		"\x1b[2m    --- SKIP\x1b[0m",
+		"\x1b[2mSKIP\x1b[0m",
+		"\x1b[2m    SKIP\x1b[0m",
+		"\x1b[2m-test.shuffle=1234\x1b[0m",
+		"\x1b[32m--- PASS\x1b[0m",
+		"\x1b[32m    --- PASS\x1b[0m",
+		"\x1b[32mPASS\x1b[0m",
+		"\x1b[32m    PASS\x1b[0m",
+		"\x1b[91m--- FAIL\x1b[0m",
+		"\x1b[91m    --- FAIL\x1b[0m",
+		"\x1b[91mFAIL\x1b[0m",
+		"\x1b[91m    FAIL\x1b[0m",
+		"\x1b[1m=== RUN\x1b[0m",
+		"\x1b[1m    === RUN\x1b[0m",
+		"\x1b[2mSKIP    ........................................................pkgn [no tests]\x1b[0m",
+		"?   \tunexpected",
+		"\x1b[32mPASS    ........................................................pkgn 1s        \x1b[0m",
+		"\x1b[91mFAIL    ...ppppppppppppppppppppppppppppppppppppppppppppppppppppppppp 1s        \x1b[0m",
+		"DONE    [SKIP: 1, PASS: 1]                                           0s        ",
+		"",
+	}, "\n")))
+
+	g.Expect(errBuf).To(Equal(""))
+}
